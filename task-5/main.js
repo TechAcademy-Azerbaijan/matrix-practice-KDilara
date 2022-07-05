cd2010b3967ce47c03b131b0a2cf80c777507ed8
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-05 20:47:08 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,8 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let str = 5;
+let mtr=[];
+for(i=0; i<=str; i++){
+  mtr.push(new Array(str).fill('*'));
+}
+for(i =0; i<mtr.length;i++ ){
+  console.log(mtr[i].join(" "))
+}
