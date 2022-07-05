660dae983389a072d35dbb89e01762a186d20788
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-05 20:45:04 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,13 +1,22 @@
-// Use this matrix for solution
-
 let matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 1, 2, 3],
-    [4, 5, 6, 7]
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 1, 2, 3],
+  [4, 5, 6, 7],
 ];
-
+let mtr=[]
 let r = 3;
 let c = 2;
 
-// Write code here. 
+for (i = 0; i < r; i++) {
+  mtr.push([])
+  for (j = 0; j < c; j++) {
+    mtr[i].push(matrix[i][j])
+   
+  }
+}
+
+
+for (i = 0; i < mtr.length; i++) {
+  console.log(mtr[i].join(" "));
+}
